8c8173baf8d9a137e59ed888c6b7183ce9eab51f
FastLite <Volodin_vlad2002@mail.ru>
2020-08-16 19:02:31 -0400

    Import of the last version of the game

---
+++ Assets/Scripts/DealOrNoDeal.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DealOrNoDeal : MonoBehaviour
+{
+
+    public Text resultTxt;
+    public Text logTxt;
+    public Text boxesLeftTxt;
+
+    public GameObject gameLevelPanelGO;
+    public GameObject gameOverPanelGO;
+
+    public int boxesMax = 4;
+    public static int[] range = { 10, 100, 1000, 10000, 100000, 1000000 };
+
+    public int box1;
+    public int box2;
+    public int box3;
+    public int box4;
+    public int box5;
+    public int box6;
+
+    public Text ten;
+    public Text hungred;
+    public Text thousand;
+    public Text tenThousand;
+    public Text hungredThousand;
+    public Text millione;
+
+    public static int[] result = shuffle(range);
+
+
+
+    public int chosenBox;
+    public int reservedBox;
+
+    private int totalChosenBoxes;
+
+    private bool isGameOver;
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (logTxt == null) this.logTxt = GameObject.Find("logTxt").GetComponent<Text>();
+        logTxt.text = "";
+        if(boxesLeftTxt ==null) this.boxesLeftTxt = GameObject.Find("boxesLeftTxt").GetComponent<Text>();
+        boxesLeftTxt.text = "Boxes left: " + boxesMax;
+    }
+
+
+    public static int[] shuffle(int[] array)
+    {
+        var rng = new System.Random();
+        int n = array.Length;
+        while (n > 1)
+        {
+            int k = rng.Next(n--);
+            int temp = array[n];
+            array[n] = array[k];
+            array[k] = temp;
+        }
+        return array;
+    }
+
+    [ContextMenu("Assigner")]
+    void AssignArray()
+    {
+
+        box1 = result[0];
+        box2 = result[1];
+        box3 = result[2];
+        box4 = result[3];
+        box5 = result[4];
+        box6 = result[5];
+        Debug.Log(box1);
+        Debug.Log(box2);
+        Debug.Log(box3);
+        Debug.Log(box4);
+        Debug.Log(box5);
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetGuessValue(int guessVal)
+    {
+        chosenBox = guessVal;
+
+    }
+    [ContextMenu("Gameplay")]
+    public void PlayNext()
+    {
+        if (reservedBox == -1 && chosenBox != -1)
+        {
+            reservedBox = chosenBox;
+            totalChosenBoxes = 1;
+        }
+        
+        if (isGameOver) return;
+        if (totalChosenBoxes < boxesMax)
+        {
+            if (reservedBox != chosenBox)
+            {
+                logTxt.text = result[chosenBox - 1] + "$ disappears";
+                leftBoxClearing(chosenBox);
+                GameObject ob = GameObject.Find("Box" + chosenBox);
+                ob.SetActive(false);
+                totalChosenBoxes++;
+            }
+            else
+            {
+                logTxt.text = "you chosed box № " + chosenBox;
+            }
+
+
+            
+
+            int boxesLeft = boxesMax - totalChosenBoxes;
+            boxesLeftTxt.text = "Boxes left to chose: " + boxesLeft;
+        }
+
+        if (!isGameOver && totalChosenBoxes == boxesMax)
+        {
+            isGameOver = true;
+
+            gameLevelPanelGO.SetActive(false);
+            gameOverPanelGO.SetActive(true);
+            resultTxt.text = "You won with box№ " + reservedBox + "\n Your total: " + result[reservedBox - 1] + "$";
+        }
+    }
+    [ContextMenu("Restart game")]
+    public void RestartGame()
+    {
+        totalChosenBoxes = 0;
+        isGameOver = false;
+        AssignArray();
+        resultTxt.text = "";
+    }
+    private void leftBoxClearing(int chosenBox)
+    {
+        if (result[chosenBox-1] == 10) ten.text = "";
+        if (result[chosenBox-1] == 100) hungred.text = "";
+        if (result[chosenBox-1] == 1000) thousand.text = "";
+        if (result[chosenBox-1] == 10000) tenThousand.text = "";
+        if (result[chosenBox-1] == 100000) hungredThousand.text = "";
+        if (result[chosenBox-1] == 1000000) millione.text = "";
+    }
+
+}
---
+++ Assets/Scripts/GuessTheDoor.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Collections;
+using UnityEngine.UI; // this is to make referecnes
+
+public class GuessTheDoor : MonoBehaviour
+
+{
+
+    public Text resultTxtField; // this is too hook code to the UI
+    public Text guessesLeftTxtField;
+
+    public GameObject gameLevelPanelGO;
+    public GameObject gameOverPanelGO;
+
+    //DOOR1 DOOR2 DOOR3 DOOR4 DOOR5
+
+    private int maxDoors = 5;
+    public int maxGuesses = 2; // max number of guesses
+
+    public int chosenDoor; // player selected door
+
+
+    //[SerializeField] 
+    private int randomDoor; // script will generate random door with prize
+
+    private int totalGuesses; //keeping the track of guesses
+
+    private bool isGameOver;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateRandomDoor();
+        resultTxtField.text = "Welcome ! Now chose the door";
+    }
+
+    //Our first function as it known if c#
+    [ContextMenu("Test Rand Generator")]
+    void GenerateRandomDoor()
+    {
+        randomDoor = UnityEngine.Random.Range(1, maxDoors + 1);
+        //resultTxtField.text = "Yay new game ! This is a test " + randomDoor;
+        Debug.Log("Yay new game ! This is a test " + randomDoor);
+        guessesLeftTxtField.text = (maxGuesses - totalGuesses) + " guesses left";
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    public void SetGuessValue(int guessVal) //this function will be called from the UI, it will pass the value we enter int he UI to the chosenDoor variable
+    {
+        chosenDoor = guessVal;
+        //PlayNext();
+
+    }
+
+    [ContextMenu("Test Gameplay")]
+    public void PlayNext()
+    {
+
+        guessesLeftTxtField.text = (maxGuesses - totalGuesses - 1)  + " guesses left";
+
+        if (isGameOver) return;
+        if (totalGuesses < maxGuesses)
+        {
+            if (chosenDoor == randomDoor)
+            {
+                resultTxtField.text = "you guesssed the right door! treasure is yours.";
+                Debug.Log("you guesssed the right door! treasure is yours.");
+                isGameOver = true; // game is over, it's important
+
+                gameLevelPanelGO.SetActive(false);
+                gameOverPanelGO.SetActive(true);
+            }
+            else
+            {
+                resultTxtField.text = "Wrong. Try again ? ";
+                Debug.Log("Wrong. Try again ? ");
+
+            }
+
+            totalGuesses++;
+        }
+        // || = or
+        // && = and
+        // ! = not
+        if (isGameOver != true && totalGuesses == maxGuesses) //isGameOver !=true = !isGameOver
+        {
+            //what to do ?
+            //end game
+            //let the user know about ending
+            //
+            resultTxtField.text = "Game Over -- maximum reached The right door was door number " + randomDoor;
+            Debug.Log("Game Over -- maximum reached");
+            isGameOver = true;
+            Debug.Log("The right door was door number " + randomDoor);
+
+            gameLevelPanelGO.SetActive(false);
+            gameOverPanelGO.SetActive(true);
+        }
+    }
+
+    [ContextMenu("Test restar game")]
+    public void RestartGame()
+    {
+         if (isGameOver == true)
+        {
+            isGameOver = false;
+            totalGuesses = 0;
+            randomDoor = 0;
+            GenerateRandomDoor();
+            resultTxtField.text = "Welcome ! Now chose the door";
+
+            gameLevelPanelGO.SetActive(true);
+            gameOverPanelGO.SetActive(false);
+        }
+    }
+
+}
+
